dcd29102750cf319e2682224bb2057e49dc0791c
Ilya Zyabirov <izyabirov@ptsecurity.com>
2024-05-28 17:49:24 +0300

    stage 3

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -11,20 +11,22 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	// fmt.Println("Logs from your program will appear here!")
 
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	in, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		panic(fmt.Sprintf("can't read input: %v", err))
+		// Wait for user input
+		in, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			panic(fmt.Sprintf("can't read input: %v", err))
+		}
+
+		in = strings.TrimSpace(in)
+
+		split := strings.Split(in, " ")
+		if len(split) < 1 {
+			panic(fmt.Sprintf("invalid input len: %d", len(split)))
+		}
+
+		fmt.Fprintf(os.Stdout, "%s: command not found\n", split[0])
 	}
-
-	in = strings.TrimSpace(in)
-
-	split := strings.Split(in, " ")
-	if len(split) < 1 {
-		panic(fmt.Sprintf("invalid input len: %d", len(split)))
-	}
-
-	fmt.Fprintf(os.Stdout, "%s: command not found\n", split[0])
 }
